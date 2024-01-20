25560bfae00f6e88fe274583a56b3dd412945564
roh84 <154079396+roh84@users.noreply.github.com>
2024-01-20 22:11:43 +0900

    4
    4

---
+++ ts/joinmembership.ts
@@ -0,0 +1,170 @@
+//@ts-nocheck
+
+//가입 체크를 위한 변수
+let namecheck = false;
+let idcheck = false;
+let pwcheck = false;
+
+//기본 패치 아작스 함수
+let form: FormData = new FormData();
+async function loginajax(url: string, option?: any) {
+  return fetch(url, option).then((res) => res.json());
+}
+
+//아이디 중복확인
+
+async function logincheckID() {
+  loginajax("/api/select/checkapi.jsp", {
+    method: "POST",
+    body: form,
+  }).then((json) => {
+    let userIds = json.userids.map((data) => data.userId);
+    let inputForUserId = document.querySelector("input[name='userid']").value;
+    let checkID = userIds.find((lang) => lang === inputForUserId);
+    if (inputForUserId.length <= 4) {
+      idcheck = false;
+      return Swal.fire({
+        title: "",
+        text: "5자 이상 적어주세요",
+        icon: "warning",
+      });
+    }
+    if (checkID !== undefined) {
+      idcheck = false;
+      Swal.fire({
+        title: "",
+        text: "동일한 아이디가 있습니다",
+        icon: "warning",
+      });
+    } else {
+      idcheck = true;
+      Swal.fire({
+        title: "",
+        text: "사용가능한 아이디 입니다",
+        icon: "success",
+      });
+    }
+  });
+}
+//닉네임중복 확인
+async function logincheckName() {
+  loginajax("/api/select/checkapi.jsp", {
+    method: "POST",
+    body: form,
+  }).then((json) => {
+    let userNicknames = json.userNicknames.map((datas) => datas.userNickname);
+    let inputForUserNickname = document.querySelector(
+      "input[name='nickname']"
+    ).value;
+    let checkNmae = userNicknames.find((lang) => lang === inputForUserNickname);
+    if (inputForUserNickname.length <= 1) {
+      idcheck = false;
+      return Swal.fire({
+        title: "",
+        text: "2자 이상 적어주세요",
+        icon: "warning",
+      });
+    }
+    if (checkNmae !== undefined) {
+      namecheck = false;
+      Swal.fire({
+        title: "",
+        text: "동일한 닉네임이 있습니다",
+        icon: "warning",
+      });
+    } else {
+      namecheck = true;
+      Swal.fire({
+        title: "",
+        text: "사용가능한 닉네임 입니다",
+        icon: "success",
+      });
+    }
+  });
+}
+
+// 비밀번호 일치 여부 확인
+async function checkPasswordMatch() {
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("passwordcheck").value;
+  let passwordMatchStatus = document.getElementById("passwordMatchStatus");
+  if (password && confirmPassword) {
+    if (password === confirmPassword) {
+      passwordMatchStatus.innerText = "비밀번호 일치";
+      passwordMatchStatus.style.color = "green";
+      pwcheck = true;
+    } else {
+      passwordMatchStatus.innerText = "비밀번호 불일치";
+      passwordMatchStatus.style.color = "red";
+      pwcheck = false;
+    }
+  } else {
+    passwordMatchStatus.innerText = "";
+    pwcheck = false;
+  }
+}
+
+//닉네임
+function checkAndDisableButton() {
+  let name = document.getElementById("name").value;
+  let nickname = document.getElementById("nickname").value;
+  if (name && nickname && (idcheck && namecheck) === true) {
+    document.getElementById("signupButton").disabled = true;
+  } else {
+    document.getElementById("signupButton").disabled = false;
+  }
+}
+
+//올채크
+document
+  .getElementById("signupForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let nickname = document.getElementById("nickname").value;
+
+    if (name && nickname && namecheck && idcheck && pwcheck) {
+      this.submit();
+    } else {
+      let errorMessage = "";
+      if (!name) {
+        errorMessage += "이름이 올바르지 않습니다.\n";
+      }
+
+      if (!nickname) {
+        errorMessage += "닉네임이 올바르지 않습니다.\n";
+      }
+
+      if (!namecheck) {
+        errorMessage += "닉네임 중복 체크 해주세요.\n";
+      }
+
+      if (!idcheck) {
+        errorMessage += "아이디 중복 체크 해주세요.\n";
+      }
+
+      if (!pwcheck) {
+        errorMessage += "비밀번호 체크가 실패했습니다.\n";
+      }
+
+      Swal.fire({
+        title: "",
+        text: errorMessage,
+        icon: "warning",
+      });
+    }
+  });
+
+document.getElementById("nickname").addEventListener("input", () => {
+  namecheck = false;
+});
+document.getElementById("userid").addEventListener("input", () => {
+  idcheck = false;
+});
+document
+  .getElementById("password")
+  .addEventListener("input", checkPasswordMatch);
+document
+  .getElementById("passwordcheck")
+  .addEventListener("input", checkPasswordMatch);
---
+++ ts/messagebox.ts
@@ -0,0 +1,205 @@
+//@ts-nocheck
+
+async function messgeajax(url: string, option?: any) {
+    return fetch(url, option).then((res) => res.json());
+   
+  } 
+
+
+async function messgedelet(notenum: String, endpoint: String) {
+  let Sendform: FormData = document.querySelector(`#${endpoint}form${notenum}`);
+  console.log(Sendform);
+  let formData = new FormData(Sendform);
+  let jsonData = {
+    seenum: formData.get("seenum"),
+    notenum: formData.get("notenum"),
+    // 다른 필드들 추가
+  };
+  await messgeajax(`/Deletmessage/${endpoint}`, {  
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(jsonData),
+  });
+}
+
+
+async function messgedeletAll(endpoint: String) {
+  let Sendform: FormData = document.querySelector(`#${endpoint}form`);
+  let formData = new FormData(Sendform);
+  let jsonData = {
+    seenums: formData.get("seenums"),
+    notenums: formData.get("notenums"),
+    // 다른 필드들 추가
+  };
+  await messgeajax(`/Deletmessage/${endpoint}`, {  
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(jsonData),
+  });
+}
+
+
+async function sendDelet(notenum:String){
+
+  let summaryElement = document.querySelector(`#sendsum${notenum}`);
+    summaryElement.parentNode.style.display = 'none';
+    await messgedelet(notenum,"send");
+}
+
+async function recieveDelet(notenum:String){
+ 
+  let summaryElement = document.querySelector(`#receivesum${notenum}`);
+    summaryElement.parentNode.style.display = 'none';
+    await messgedelet(notenum,"recieve");
+}
+
+
+async function sendDeletAll(notenumString:String){ 
+  let notenumArray = JSON.parse(notenumString);
+
+  // 배열의 각 원소에 대해 반복
+  for (let i = 0; i < notenumArray.length; i++) {
+      let currentNotenum = notenumArray[i];
+
+      // 작업 수행
+      let summaryElement = document.querySelector(`#sendsum${currentNotenum}`);
+      if (summaryElement) {
+          summaryElement.parentNode.style.display = 'none';      
+      }
+  }
+    await messgedeletAll("sendAll");
+}
+
+async function recieveDeletAll(notenumString:String){ 
+
+  let notenumArray = JSON.parse(notenumString);
+  for (let i = 0; i < notenumArray.length; i++) {
+      let currentNotenum = notenumArray[i];
+      let summaryElement = document.querySelector(`#receivesum${currentNotenum}`);
+      if (summaryElement) {
+          summaryElement.parentNode.style.display = 'none';      
+      }
+  }
+    await messgedeletAll("receiveAll");
+}
+
+async function writemessage(){
+   Nicknamecheck();
+
+}
+
+////////////////////////////////////////
+
+
+let intervalId;
+
+function startInterval() {
+    intervalId = setInterval(function() {
+       location.reload();
+    }, 10000); // 10초마다 실행
+}
+
+
+function stopInterval() {
+  clearInterval(intervalId); // 현재 타이머를 중지
+}
+
+// 마우스가 특정 화면 위에 있을 때
+document.getElementById("modal").addEventListener('mouseover', function() {
+    stopInterval();
+    startInterval();   
+});
+
+// 마우스가 특정 화면 위에서 벗어날 때
+document.getElementById("modal").addEventListener('mouseout', function() {
+  stopInterval();
+  startInterval();
+});
+
+// 채팅 입력 중일 때 
+document.getElementById("modal").addEventListener('input', function() {
+  stopInterval();
+ startInterval();
+});
+
+// 채팅 입력이 끝났을 때 
+document.getElementById("modal").addEventListener('blur', function() {
+    stopInterval();
+    startInterval();  
+});
+
+// 초기에 setInterval 시작
+startInterval();
+
+////////////////////////////////////////////////////////////////////////////////
+let form: FormData = new FormData();
+async function loginajax(url: string, option?: any) {
+    return fetch(url, option).then((res) => res.json());
+  } 
+
+async function Nicknamecheck(){
+  loginajax("/api/select/checkapi.jsp",{
+  method: "POST",
+  body: form,
+}).then((json) => {   
+  let userNicknames = json.userNicknames.map(datas => datas.userNickname); 
+  let inputForUserNickname = document.querySelector(".note-receiver").value;
+  let checkNmae = userNicknames.find(lang=>lang === inputForUserNickname);    
+     if(checkNmae == undefined){
+     return  Swal.fire({
+         title: '',
+         text: inputForUserNickname+"님은 존재하지 않습니다",
+         icon: 'warning',
+     });
+     }else{
+      let writeform: FormData = document.querySelector(`#writemessage`);
+      let writeformData = new FormData(writeform);
+      let writejsonData = {
+        sender: writeformData.get("sender"),
+        receiver: writeformData.get("receiver"),
+        content: writeformData.get("content")
+      };
+       messgeajax("/MessageServlet",{  
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(writejsonData),
+      });   
+      setTimeout(() => {
+        location.reload();
+      }, 200);
+     }
+});
+}
+
+// let writeform: FormData = document.querySelector(`#writemessage`);
+// let writeformData = new FormData(writeform);
+// let writejsonData = {
+//   sender: writeformData.get("sender"),
+//   receiver: writeformData.get("receiver"),
+//   content: writeformData.get("content")
+// };
+//  messgeajax("/MessageServlet",{  
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify(writejsonData),
+// });
+
+// setTimeout(() => {
+//   location.reload();
+// }, 200);
+
+
+
+
+
+
+
+
